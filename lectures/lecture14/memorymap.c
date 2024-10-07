c45a56a71f1f1eac17319f7e06df0f798817389b
Lorenzo De Carli <lorenzo.decarli@ucalgary.ca>
2024-10-07 15:38:04 +0000

    Lecture 14

---
+++ lectures/lecture14/memorymap.c
@@ -0,0 +1,14 @@
+   #include <stdio.h>
+   #include <stdlib.h>
+   #include <stdlib.h>
+   
+   int main() {
+     int x = 777;
+     int* y = (int*)malloc(sizeof(int));
+     printf("location of code: %p\n", (void *) main);
+     printf("location of heap: %p\n", (void *) malloc(1));
+     printf("location of stack: %p\n", (void *) &x); 
+     printf("location of printf: %p\n", (void *) printf);
+     printf("location of malloc: %p\n", (void *) malloc);
+     return 0;
+   }
---
+++ lectures/lecture14/stackexample.c
@@ -0,0 +1,7 @@
+void foo(int a, int b, int c) {
+    char* buf[16];
+}
+
+int main() {
+    foo(1,2,3);
+}
